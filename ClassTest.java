7e0bddc955a3ea6f18f750d1e3e55dab9a7dad81
fukushimayuka <130010510+fukushimayuka@users.noreply.github.com>
2023-04-14 16:10:41 +0900

    Add files via upload

---
+++ ClassTest.java
@@ -0,0 +1,30 @@
+class ClassTest {
+    public static void main(String[] args){
+
+        String nekoName = "たまちゃん";
+        String dogName = "たろう";
+        String bardName = "ピーコ";
+
+        System.out.println("にゃー");
+
+        System.out.println("わんわん");
+
+        System.out.println("チュンチュン");
+
+    }
+}
+
+class Animal{
+    
+    String name;
+    String voice;
+
+    public Animal(String n, String v){
+        this.name
+    }
+
+    public void cry(){
+        system.out.println(this.name);
+    }
+
+}
